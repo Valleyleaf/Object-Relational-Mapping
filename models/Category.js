c05284090c23508a011cf62023b2d1c0000e1978
Aston Dahllöf <aston_dahllof@outlook.com>
2023-11-13 16:51:02 -0400

    Added category models

--- models/Category.js
+++ models/Category.js
@@ -10,8 +10,67 @@ Category.init(
       type:DataTypes.INTEGER,
       primaryKey:true,
       autoIncrement:true,
+      allownull: false,
+  }
+  
   },
+  {
+    category_name: {
+      type:DataTypes.STRING,
+      allownull: false,
+  }
   },
+  {
+    product: {
+      id: Category.id,
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true,
+      allownull: false,
+  }
+  },
+  {
+    product_name: {
+      type:DataTypes.STRING,
+      allownull: false,
+  }
+  },
+  {
+    price: {
+      type:DataTypes.DECIMAL,
+      allownull: false,
+      validate: {
+        isDecimal: true,
+      },
+  }
+  },
+  {
+    stock: {
+      type:DataTypes.INTEGER,
+      allownull: false,
+      default: 10,
+      validate: {
+        isInt: true,
+      }
+  }
+  },
+  {
+    category_id: {
+      type:DataTypes.INTEGER,
+      //Need to reference category ID. Figure out how.
+      }
+  },
+  {
+    Tag: {
+      id: Category.id,
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true,
+      allownull: false,
+      //Not sure if Category.id is the correct way here.
+  }
+  },
+  
   {
     sequelize,
     timestamps: false,
