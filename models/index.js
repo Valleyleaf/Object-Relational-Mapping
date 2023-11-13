e36fdb3c19642f27616ccb0dbf5677df3f30d9e9
Aston Dahllöf <aston_dahllof@outlook.com>
2023-11-13 15:18:02 -0400

    Updated index in models.

--- models/index.js
+++ models/index.js
@@ -11,6 +11,19 @@ const ProductTag = require('./ProductTag');
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+});
+Category.hasMany(Product, {
+  foreignKey: 'id',
+});
+Tag.hasMany(Product, {
+  foreignKey: 'id',
+});
+Product.belongsTo(Tag, {
+  foreignKey: 'id',
+});
+
 
 module.exports = {
   Product,
